dc046c9cb553161b21f7cc55f9060c26e8bc655b
mostafamahmoud96 <moustafa.elagha96@gmail.com>
2021-12-30 03:13:15 +0200

    add database Name

--- docker-compose/mysql/init_db.sql
+++ docker-compose/mysql/init_db.sql
@@ -1,9 +1,3 @@
-DROP TABLE IF EXISTS `files`;
+DROP TABLE IF EXISTS `docker_test`;
 
-CREATE TABLE `files` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `file_path` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
+CREATE DATABASE `docker_test`;
