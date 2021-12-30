64c388c8c3fe91f08854894165d54beffca94fd4
mostafamahmoud96 <moustafa.elagha96@gmail.com>
2021-12-30 15:09:47 +0200

    add file validation

--- app/Http/Controllers/Api/FileController.php
+++ app/Http/Controllers/Api/FileController.php
@@ -7,41 +7,44 @@ use Illuminate\Http\Request;
 use App\Models\File;
 
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class FileController extends Controller
 {
     public function upload_file(Request $request)
     {
-        if ($request->file) {
-            $fileModel = new File();
-            $uploaded_file = $request->file;
-            $path = public_path("uploads");
-            $withoutExt = preg_replace('/\\.[^.\\s]{3,4}$/', '', $request->file->getClientOriginalName()); //remove extenison
-            $fileName = md5($withoutExt) . '.' . $uploaded_file->getClientOriginalExtension();
-            if (File::where('name', md5($withoutExt))->exists()) {
-                $files = scandir($path);
-                $files = array_diff(scandir($path), array('.', '..'));
-                $matchingFiles = preg_grep('{' . $fileName . '}', $files);
-                if ($matchingFiles) {
-                    foreach ($matchingFiles as $file) {
-                        return response()->json(['status' => 200, 'message' => 'you already uploaded the file before', 'file' =>
-                        asset("/uploads/$file")]);
-                    }
+        $validator = Validator::make($request->all(), [
+            'file' => 'required|mimes:jpeg,pdf,png,csv'
+        ]);
+        if ($validator->fails()) {
+            return response()
+                ->json(['status' => 415, 'message' => 'You Must upload valid Type file']);
+        }
+        $fileModel = new File();
+        $uploaded_file = $request->file;
+        $path = public_path("uploads");
+        $withoutExt = preg_replace('/\\.[^.\\s]{3,4}$/', '', $request->file->getClientOriginalName()); //remove extenison
+        $fileName = md5($withoutExt) . '.' . $uploaded_file->getClientOriginalExtension();
+        if (File::where('name', md5($withoutExt))->exists()) {
+            $files = scandir($path);
+            $files = array_diff(scandir($path), array('.', '..'));
+            $matchingFiles = preg_grep('{' . $fileName . '}', $files);
+            if ($matchingFiles) {
+                foreach ($matchingFiles as $file) {
+                    return response()->json(['status' => 200, 'message' => 'you already uploaded the file before', 'file' =>
+                    asset("/uploads/$file")]);
                 }
-            } elseif (!File::where('name', md5($withoutExt))->exists()) {
-                $fileModel->name = md5($withoutExt);
-                $uploaded_file->move($path, $fileName);
-                $fileModel->file_path = '/uploads/' . $fileName;
-                $fileModel->save();
-
-                return response()->json(['status' => 200, 'message' => 'File uploaded successfully']);
-            } else {
-                return response()
-                    ->json(['status' => 404, 'message' => 'You Must upload type file']);
             }
+        } elseif (!File::where('name', md5($withoutExt))->exists()) {
+            $fileModel->name = md5($withoutExt);
+            $uploaded_file->move($path, $fileName);
+            $fileModel->file_path = '/uploads/' . $fileName;
+            $fileModel->save();
+
+            return response()->json(['status' => 200, 'message' => 'File uploaded successfully']);
         } else {
             return response()
-                ->json(['status' => 404, 'message' => 'some thing went wrong']);
+                ->json(['status' => 400, 'message' => 'You Must upload type file']);
         }
     }
 
@@ -55,7 +58,7 @@ class FileController extends Controller
                 return response()->json(['status' => 200, 'file_name' => $request->name, 'file' => asset($file->file_path)]);
             } else {
                 return response()
-                    ->json(['status' => 404, 'message' => 'No Matching File']);
+                    ->json(['status' => 200, 'message' => 'No Matching File']);
             }
         } else {
             return response()
@@ -76,11 +79,9 @@ class FileController extends Controller
                 return response()->json(['status' => 200, 'message' => 'File is Deleted Successfully']);
             } else {
                 return response()
-                    ->json(['status' => 404, 'message' => 'file does not exist']);
+                    ->json(['status' => 200, 'message' => 'file does not exist']);
             }
-        }
-        else
-        {
+        } else {
             return response()
                 ->json(['status' => 404, 'message' => 'input file name can not be empty']);
         }
